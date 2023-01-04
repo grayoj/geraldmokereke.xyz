cd1cb60c890ae5b107294e9d54152e74e1a67c6f
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2023-01-04 10:36:56 +0100

    Published 01/04/2023 10:36:56

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "1. Clearing public folder"
+rm -rf public/*
+echo "graysnotes.github.io" > public/CNAME
+
+echo "\n2. Generating site"
+hugo
+
+echo "\n3. Publishing to GitHub Pages"
+cd public
+git add --all
+git commit -m "Published $(date)"
+git push origin gh-pages
+cd ..
