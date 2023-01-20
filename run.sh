e490e144cdec9cbe5a78f139056131ffa1b27490
grayoj <Mgeraldoj07@gmail.com>
2023-01-20 18:07:14 +0100

    fix(fix shell script install): wrong clone link
    Fixed the github repo link

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Clone and Serve"
-git clone --recurse-submodules https://github.com/grayoj/gnotes
+git clone --recurse-submodules https://github.com/grayoj/gnotes.git
 Hugo server hugo server -D
